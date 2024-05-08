32ad679fe71e1e4ec463706294133845ff514db0
Shavinda Silva <shavinda34@gmail.com>
2024-05-08 14:51:11 +0930

    feat:Update exception handle when user is not authenticated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,6 +30,12 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->json([
                 'message' => $e->getMessage(),
             ], ResponseAlias::HTTP_UNAUTHORIZED);
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], ResponseAlias::HTTP_NOT_FOUND);
         });
 
     })->create();
