79f38bb7036607e079c72e043f71b98e9e84365c
Shavinda Silva <shavinda34@gmail.com>
2024-05-09 11:46:26 +0930

    build:Add Pest

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,16 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\StudentController;
-use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 
 Route::middleware(['auth:sanctum'])->group(function (): void {
-    Route::get('/user', [AuthController::class, 'show']);
+    Route::get('/user', [AuthController::class, 'show'])->name('auth.show');
     Route::get('/logout', [AuthController::class, 'revokeTokens']);
 
     Route::resource('/classes', CourseController::class)->only(['index', 'show']);
     Route::resource('/students', StudentController::class)->only(['show']);
-    Route::get('/classes/{class}/students', [CourseController::class, 'students'])->can('list', Course::class);
+    Route::get('/classes/{class}/students', [CourseController::class, 'students'])->name('class.students');
 });
